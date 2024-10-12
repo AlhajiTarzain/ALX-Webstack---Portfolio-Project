5d07f96752d8dce8e71e238bc91d1dc119fc0974
AlhajiTarzain <dredeen21@gmail.com>
2024-10-12 17:24:28 +0000

    forms created classes for login ,sign up , and recippe submission

---
+++ app/forms.py
@@ -0,0 +1,32 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, TextAreaField, SubmitField, SelectField ,FileField
+from wtforms.validators import DataRequired, Email, EqualTo, Length
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username',validators=[DataRequired(),Length(min=5, max=15)])
+    email = StringField('Email',validators=[DataRequired(),Email()])
+    password = PasswordField('Password',validators=[DataRequired()])
+    confirm_password =  PasswordField('Confirm Password',validators=[DataRequired(),EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email',validators=[DataRequired(),Email()])
+    password = PasswordField('Password',validators=[DataRequired()])
+    submit = SubmitField ('Log In')
+
+class RecipeForm(FlaskForm):
+    title = StringField('Title',validators=[DataRequired(),Length(min=2, max=100)])
+    category = SelectField('Category', 
+                           choices=[('appetizer', 'Appetizer'), 
+                                    ('main_course', 'Main Course'), 
+                                    ('dessert', 'Dessert')],
+                           validators=[DataRequired()])
+    difficulty = SelectField('Difficulty Level',
+                             choices=[('easy', 'Easy'),
+                                      ('medium', 'Medium'),
+                                      ('hard', 'Hard')],
+                             validators=[DataRequired()])
+    origin = StringField('Country of Origin')
+    ingredients = TextAreaField('Ingredients',  validators=[DataRequired()])
+    image = FileField('Recipe Image')
+    submit = SubmitField('Submit Recipe')
